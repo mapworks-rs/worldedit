4c75e78a53f62b91780063ea8b34ae41dcb07168
sagan <camjanssen120@gmail.com>
2020-10-14 13:22:52 -0600

    rotate with non repeating quaternion

--- src/clipboard/clipboard.rs
+++ src/clipboard/clipboard.rs
@@ -27,28 +27,43 @@ pub mod operations {
         impl Operation for Rotate {
             fn perform(&self, clipboard: Option<&mut Clipboard>, selection: Option<&mut Selection>) {
                 if let Some(clip) = clipboard {
+
+                    let quat = Quat::from_rotation_y(self.y_deg.to_radians() as f32)
+                        .mul_quat(Quat::from_rotation_x(self.x_deg.to_radians() as f32))
+                        .mul_quat(Quat::from_rotation_z(self.z_deg.to_radians() as f32));
+
                     for block in &mut clip.data {
-                        rotate(block.pos.borrow_mut(), self.x_deg.to_radians(),
-                               self.y_deg.to_radians(), self.z_deg.to_radians())
+                        rotate(block.pos.borrow_mut(), quat);
                     }
                 }
             }
 
             fn undo(&self, clipboard: Option<&mut Clipboard>, selection: Option<&mut Selection>) {
                 if let Some(clip) = clipboard {
+
+                    let quat = Quat::from_rotation_y(-self.y_deg.to_radians() as f32)
+                        .mul_quat(Quat::from_rotation_x(-self.x_deg.to_radians() as f32))
+                        .mul_quat(Quat::from_rotation_z(-self.z_deg.to_radians() as f32));
+
                     for block in &mut clip.data {
-                        rotate(block.pos.borrow_mut(), -self.x_deg.to_radians(),
-                               -self.y_deg.to_radians(), -self.z_deg.to_radians())
+                        rotate(block.pos.borrow_mut(), quat);
                     }
                 }
             }
         }
 
-        fn rotate(block_pos: &mut BlockPosition, x_deg: f64, y_deg: f64, z_deg: f64) {
+        fn rotate_deg(block_pos: &mut BlockPosition, x_deg: f64, y_deg: f64, z_deg: f64) {
             let vec = Quat::from_rotation_y(y_deg.to_radians() as f32)
                 .mul_quat(Quat::from_rotation_x(x_deg.to_radians() as f32))
                 .mul_quat(Quat::from_rotation_z(z_deg.to_radians() as f32))
                 .mul_vec3a(Vec3A::new(block_pos.x as f32, block_pos.y as f32, block_pos.z as f32));
+            block_pos.x = vec.x() as i32;
+            block_pos.y = vec.y() as i32;
+            block_pos.z = vec.z() as i32;
+        }
+
+        fn rotate(block_pos: &mut BlockPosition, quaternion: Quat) {
+            let vec = quaternion.mul_vec3a(Vec3A::new(block_pos.x as f32, block_pos.y as f32, block_pos.z as f32));
             block_pos.x = vec.x() as i32;
             block_pos.y = vec.y() as i32;
             block_pos.z = vec.z() as i32;
